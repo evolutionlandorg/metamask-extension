09aab0af1f4fe9e2076b564c7f0b9b3f84ab907e
bitpshr <mail@bitpshr.net>
2018-07-02 12:19:19 -0400

    Fix flat chrome tests

--- development/mock-dev.js
+++ development/mock-dev.js
@@ -76,7 +76,6 @@ const controller = new MetamaskController({
   showUnconfirmedMessage: noop,
   unlockAccountMessage: noop,
   showUnapprovedTx: noop,
-  platform: {},
   // initial state
   initState: firstTimeState,
 })
